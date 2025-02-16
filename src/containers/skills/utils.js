4cbb1d2b4e77e03717af6ed2559720d09de48c0f
bollytn <ps3online85@gmail.com>
2025-02-16 03:15:55 +0100

    Add skills section with animated progress bars and data structure

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,53 @@
+export const skillsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName: "CSS",
+                percentage: "90",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "80",
+            },
+        ],
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "NODEJS",
+                percentage: "70",
+            },
+            {
+                skillName: "PYTHON",
+                percentage: "65",
+            },
+            {
+                skillName: "KOTLIN",
+                percentage: "35",
+            },
+        ],
+    },
+    {
+        label: "DATABASES",
+        data: [
+            {
+                skillName: "SQL",
+                percentage: "70",
+            },
+            {
+                skillName: "MONGO DB",
+                percentage: "65",
+            },
+            {
+                skillName: "MY SQL",
+                percentage: "85",
+            },
+        ],
+    },
+];
